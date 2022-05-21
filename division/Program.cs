7f8772584db830dfe80889935a586ced2c8552f6
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 01:01:59 -0700

    Agregar archivos de proyecto.

---
+++ division/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 5;
+
+            Console.WriteLine(num1 / num2);
+            Console.ReadKey();
+
+        }
+    }
+}
